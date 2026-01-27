9c6913614f05af0b813348723f02a701c7e7ce1f
Mihail Vorobyev <1viklover@gmail.com>
2026-01-27 18:10:39 +0500

    Initial commit

---
+++ src/lr1/Example6.java
@@ -0,0 +1,16 @@
+package lr1;
+
+import java.util.Scanner;
+public class Example6 {
+    public static void main(String[] args) {
+        try(var in = new Scanner(System.in)) {
+            System.out.println("Input lastname: ");
+            var lastname = in.nextLine();
+            System.out.println("Input firstname: ");
+            var firstname = in.nextLine();
+            System.out.println("Input patronymic: ");
+            var patronymic = in.nextLine();
+            System.out.println("Hello: " + lastname + " " + firstname + " " + patronymic);
+        }
+    }
+}
---
+++ src/lr1/Example7.java
@@ -0,0 +1,15 @@
+package lr1;
+
+import java.util.Scanner;
+public class Example7 {
+    public static void main(String[] args) {
+        try(var in = new Scanner(System.in)) {
+            System.out.println("Input name: ");
+            var name = in.nextLine();
+            System.out.println("Input age: ");
+            var age = in.nextInt();
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+        }
+    }
+}
